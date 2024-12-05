616cd03ec5cc2999fc9509803e0a5ed639ba9182
ONLY1gb <gautam.bhawsar8269@gmail.com>
2024-12-05 12:24:06 +0530

    commit changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import './App.css'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Location_Map from './components/Location_Map/Location_Map'
 
 const App = () => {
   return (
     <div className='app'>
-
+      <Location_Map></Location_Map>
     </div>
   )
 }
